7f2f39dc986af7072bb22c367457730a0674f4a1
Klimov <nikolai.klimov@edicenter.de>
2024-05-02 14:22:01 +0200

    Add project files.

---
+++ MailMessageExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Net.Mail;
+using System.Reflection;
+using System.Text;
+
+public static class MailMessageExtensions
+{
+
+
+
+    /// <summary>
+    /// Internet Message Format (IMF) is the standardized ASCII-based syntax required by SMTP for all 
+    /// email message bitstreams used by a message transfer agent, sometimes referred to as a mail transfer agent or MTA, 
+    /// when moving messages between computers. IMF is standardized by RFC 5322.
+    /// 
+    /// https://www.loc.gov/preservation/digital/formats/fdd/fdd000393.shtml
+    /// 
+    /// IMF can be save to an EML file:
+    /// EML, short for electronic mail or email, is a file extension for an email message saved to a file in 
+    /// the Internet Message Format protocol for electronic mail messages. 
+    /// It is the standard format used by Microsoft Outlook Express as well as some other email programs.    
+    /// Since EML files are created to comply with industry standard RFC 5322, 
+    /// EML files can be used with most email clients, servers and applications. 
+    /// 
+    /// Die EML-Dateierweiterung wird verwendet, um E-Mail-Nachrichten im RFC-822-Format zu speichern. 
+    /// Viele Anwendungen wie Microsoft Outlook, Thunderbird, Apple Mail, Windows Mail und 
+    /// andere verwenden das EML-Format zum Exportieren und Importieren von E-Mails. 
+    /// </summary>
+    public static string ToInternetMessageFormat(this MailMessage message)
+    {
+        var assembly = typeof(SmtpClient).Assembly;
+        var mailWriterType = assembly.GetType("System.Net.Mail.MailWriter");
+
+        using (var memoryStream = new MemoryStream())
+        {
+            // Get reflection info for MailWriter contructor
+            var mailWriterContructor = mailWriterType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { typeof(Stream) }, null);
+
+            // Construct MailWriter object with our FileStream
+            var mailWriter = mailWriterContructor.Invoke(new object[] { memoryStream });
+
+            // Get reflection info for Send() method on MailMessage
+            var sendMethod = typeof(MailMessage).GetMethod("Send", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            // Call method passing in MailWriter
+            sendMethod.Invoke(message, BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { mailWriter, true, true }, null);
+
+            // Finally get reflection info for Close() method on our MailWriter
+            var closeMethod = mailWriter.GetType().GetMethod("Close", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            // Call close method
+            closeMethod.Invoke(mailWriter, BindingFlags.Instance | BindingFlags.NonPublic, null, new object[] { }, null);
+
+            return Encoding.ASCII.GetString(memoryStream.ToArray());
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+
+namespace dotnet_mailmessage_to_internet_message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteEML_BodyText();
+            WriteEML_BodyHtml();
+        }
+
+
+        static void WriteEML_BodyText()
+        {
+            string from = "james@mail.de";
+            string to = "amelia@web.com";
+            string subject = "I love you!";
+            string body = "Do you want to marry me?";
+
+            MailMessage mailMessage = new MailMessage(from, to, subject, body);
+
+            string imf = mailMessage.ToInternetMessageFormat();
+
+            DateTime ts = DateTime.Now;
+            File.WriteAllText($"{ts.ToString("yyyy-MM-dd_HH-mm")}_TxtMail_TO_{to}.eml", imf);
+        }
+
+
+        static void WriteEML_BodyHtml()
+        {
+            string from = "james@mail.de";
+            string to = "amelia@web.com";
+            string subject = "I love you!";
+            string heart = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAkQAAAJEBVAVoGQAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAFISURBVDiNpZM/S0JRGMZ/71GIiAaXhghqymtj4WKDyM2PEBZ9gPZWRyen9r5DBS2FV1EvdT+AS44FURE5mUT+OW+LhpSa2bOcw/O+v+c8yxFV5T8KDy63rrtKqJMWzLKqBIutj5uVIHgHeEgk5psLc9simsDqvZieFy1cPwKIqlLfSR4iHAPzX9HKnQ2bNIDpWg9hbejhFsiR41VOpO4mUypaAmREw5f+uTRipoimwohmx8DjwIEEJGsUNicsTZayZYDXmQOgYQS82Qto0fS6Jgc0Z+DfCJEzG+Xys7GSAdp/gDsiHMSu/CcDsF6qXIqwC3SmgVUkEy1ULwDMwO0be780aYPuxwqV84FhhqeOVz0T1ThQGwHXRDXueP7psGm+b0WLfs1GGnGBPGABK5C3kUY8WvR/BMuk31h3kwkAp1QNxu1MDJhGny82eMnIn1weAAAAAElFTkSuQmCC";
+            string img = $"<img src={heart} width=\"16\" height=\"16\" />";
+            string body = $"Do you want to {img} <strong>marry</strong> {img} me? ";
+
+            MailMessage mailMessage = new MailMessage(from, to, subject, body);
+            mailMessage.IsBodyHtml = true;
+
+
+            string imf = mailMessage.ToInternetMessageFormat();
+
+            DateTime ts = DateTime.Now;
+            File.WriteAllText($"{ts.ToString("yyyy-MM-dd_HH-mm")}_HtmlMail_TO_{to}.eml", imf);
+        }
+    }
+}
